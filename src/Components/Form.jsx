11a930b4e411df86184d0086e689fb2262c844e1
ramiromp <ramiro.marpreuss@gmail.com>
2023-09-14 18:34:26 -0300

    Estilos form, details y correccion de verificacion de email

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './Estilos/Contact.css'
+import './Estilos/ContactForm.css'
 
 
 const Form = () => {
@@ -12,11 +12,13 @@ const Form = () => {
 const [enviado, setEnviado] = useState(false)
 const [error, setError] = useState(false)
 
-const emailValidacion = (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i);
+const emailRegex = (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i);
 
 const handleSubmit = (e) => {
     e.preventDefault()
-    if(emailValidacion && cliente.nombre.length > 5) {
+    setEnviado(false);
+    setError(false);
+    if(emailRegex.test(cliente.email) && cliente.nombre.length > 5) {
         setEnviado(true)
         setError(false)
     } else {
@@ -25,13 +27,13 @@ const handleSubmit = (e) => {
 }
   
   return (
-    <div>
+    <div className="formBox">
       <form>
-        <input onChange ={(event) => setCliente({...cliente,nombre: event.target.value})} type="text" placeholder="Enter your name"/>
-        <input type="text" onChange ={(event) => setCliente({...cliente,email: event.target.value})} placeholder="Enter your email"/>
+        <input className="formPlaceholder" onChange ={(event) => setCliente({...cliente,nombre: event.target.value})} type="text" placeholder="Enter your name"/>
+        <input className="formPlaceholder" type="text" onChange ={(event) => setCliente({...cliente,email: event.target.value})} placeholder="Enter your email"/>
         <button className="formButton" onClick={handleSubmit}>Send</button>
-        {enviado && <h3>Thank you {cliente.nombre}, we will contact you as soon as possible via email. </h3>}
-        {error && <h3 style={{color: 'red'}}>There was an error, please check the information you provided.</h3>}
+        {enviado && <h3 className="formResponse">Thank you {cliente.nombre}, we will contact you as soon as possible via email. </h3>}
+        {error && <h3 className="formResponse" style={{color: 'red'}}>There was an error, please check the information you provided.</h3>}
       </form>
     </div>
   );
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Form from '../Components/Form'
-import '../Components/Estilos/Contact.css'
+import '../Components/Estilos/ContactForm.css'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
